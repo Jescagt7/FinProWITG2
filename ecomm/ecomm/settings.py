28c5b7d709967f6ad62614c04c46906a6f7ae3b1
Jessica Rusanti <135139051+Jescagt7@users.noreply.github.com>
2023-06-03 00:29:00 +0700

    Update settings.py

--- ecomm/ecomm/settings.py
+++ ecomm/ecomm/settings.py
@@ -109,9 +109,9 @@ WSGI_APPLICATION = 'ecomm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'anton_db',
+        'NAME': 'database_ecomm',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'JessicaTae',
         'HOST': 'localhost',
         'PORT': '5432',
     }
